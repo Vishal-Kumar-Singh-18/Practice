115325790eace685eef73c968ad158c5a7d71e84
Vishal <vishalkr.18singh1999@gmail.com>
2025-06-22 22:03:44 +0530

    Add count

---
+++ 01-counter/src/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+        <h1 className="text-3xl font-bold mb-1">Count: {count}</h1>
+        <button
+         onClick={() => setCount(count + 1)}
+         className="bg-[#1a1a1a] py-2 px-4 m-2 rounded-md"
+        >
+            Increase</button>
+        <button 
+        onClick={() => setCount(count - 1)}
+        className="bg-[#1a1a1a] py-2 px-4 m-2 rounded-md"
+        >Decrease</button>
+    </div>
+  )
+}
+
+export default Counter
